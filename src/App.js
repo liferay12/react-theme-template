5ccf9ba52f79f0d1aaeee681ea9e970558df3440
Lohita Sagar Mishra <lohitamishra77@gmail.com>
2023-03-24 21:07:28 +0530

    layout css done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import { Topbar } from './theme/component/TopBar';
 import { SideBar } from './theme/component/SideBar';
+import { SideBarToogle } from './theme/component/SidebarToogle';
+import { ChatBox } from './theme/component/ChatBox';
 // import {apexchart} from './theme/js';
 
 function App() {
   return (
-    <div className="App">
-      <Topbar></Topbar>
-      <SideBar></SideBar>
-    </div>
+    <body data-typography="poppins" data-theme-version="light" data-layout="vertical" data-nav-headerbg="color_4" data-headerbg="color_4" data-sidebar-style="full" data-sidebarbg="color_1" data-sidebar-position="fixed" data-header-position="fixed" data-container="wide" direction="ltr" data-primary="color_1" data-secondary="color_1">
+      <div id="main-wrapper" class="show">
+        <SideBarToogle></SideBarToogle>
+        <ChatBox></ChatBox>
+        <Topbar></Topbar>
+        <SideBar></SideBar>
+      </div>
+    </body>
   );
 }
 
