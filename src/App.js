f3f290b9ae17e23bd671d332917b7896892573cb
liferay12 <ashwani.liferay@gmail.com>
2023-03-24 10:41:55 +0530

    created folder structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import nav from './component/Navigation'
+
+function App() {
+  return (
+    <div className="App">
+      <nav></nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Navigation.jsx
@@ -0,0 +1,6 @@
+function nav() {
+    return (
+        <h1>Hello Welcome to React</h1>
+    );
+}
+export default nav;
