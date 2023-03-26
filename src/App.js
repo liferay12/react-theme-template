0dc09ac9ec24984ed8bea800b734683f06694136
liferay12 <ashwani.liferay@gmail.com>
2023-03-26 10:48:15 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import { Topbar } from './theme/component/TopBar';
 import { SideBar } from './theme/component/SideBar';
-import { SideBarToogle } from './theme/component/SidebarToogle';
+import { SideBarToggle } from './theme/component/SidebarToggle';
 import { ChatBox } from './theme/component/ChatBox';
+import ThemeRender from './theme/component/ThemeRender';
+import themeData from "./theme/theme.json"
 // import {apexchart} from './theme/js';
 
 function App() {
   return (
-    <body data-typography="poppins" data-theme-version="light" data-layout="vertical" data-nav-headerbg="color_4" data-headerbg="color_4" data-sidebar-style="full" data-sidebarbg="color_1" data-sidebar-position="fixed" data-header-position="fixed" data-container="wide" direction="ltr" data-primary="color_1" data-secondary="color_1">
-      <div id="main-wrapper" class="show">
-        <SideBarToogle></SideBarToogle>
-        <ChatBox></ChatBox>
-        <Topbar></Topbar>
-        <SideBar></SideBar>
-      </div>
-    </body>
+    // <body data-typography="poppins" data-theme-version="light" data-layout="vertical" data-nav-headerbg="color_4" data-headerbg="color_4" data-sidebar-style="full" data-sidebarbg="color_1" data-sidebar-position="fixed" data-header-position="fixed" data-container="wide" direction="ltr" data-primary="color_1" data-secondary="color_1">
+    <div >
+
+
+      <ThemeRender data={themeData}></ThemeRender> 
+      {/* <h1>hekk  k</h1>
+      {
+        themeData.section.map((data, j) => {
+
+          return (
+
+            <h1 key={j}>{data.title} jhsekjfh</h1>
+          )
+          {
+            data.option.map((item, i) => {
+
+              return (
+                <div key={i}>
+                  <h1>{item.component}</h1>
+                </div>
+              )
+
+            })
+          }
+
+        })
+      }
+ */}
+
+
+    </div>
+    //  </body>
   );
 }
 
---
+++ src/theme/component/Test1.jsx
@@ -0,0 +1,14 @@
+export const Test1 = () => {
+    return (
+        <>
+
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Library</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Data</li>
+                </ol>
+            </nav>
+        </>
+    );
+}
