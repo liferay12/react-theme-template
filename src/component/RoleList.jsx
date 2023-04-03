1c5267bbc51bb1178a77f6631867ed887624033d
Lohita Sagar Mishra <lohitamishra77@gmail.com>
2023-04-03 11:10:21 +0530

    User List, Role List in Data Table

--- src/component/RoleList.jsx
+++ src/component/RoleList.jsx
@@ -1,7 +1,99 @@
-import React from 'react'
+import axios from 'axios';
+import { Button } from 'bootstrap';
+import React, { useEffect, useRef, useState } from 'react'
+import DataTable from 'react-data-table-component';
+import { useDownloadExcel } from 'react-export-table-to-excel';
+import { Finance } from './Finance';
 
 export const RoleList = () => {
+    const [roles, setRoles] = useState([]);
+    const [search, setSearch] = useState("");
+    const [Filterroles, setFilterroles] = useState([]);
+
+    useEffect(() => {
+        async function fetchUserList() {
+            let user = await axios.get('http://localhost:9090/api/v1/role')
+            console.log("--------" + user);
+            setRoles(user.data);
+            setFilterroles(user.data);
+        };
+        fetchUserList();
+        
+    }, []);
+
+    // useEffect(() => {
+    //     const result = roles.filter(user => {
+    //         return user.name.toLowerCase().match(search.toLowerCase());
+    //     })
+    //     setFilterroles(result);
+    //     console.log("-------++++------" + roles);
+    // }, [search]);
+
+    const column = [
+        {
+            name: "User Id",
+            selector: (row) => row.roleId,
+            sortable: true
+        },
+        {
+            name: "Full Name",
+            selector: (row) => row.name+row.title,
+            sortable: true
+        },
+        {
+            name: "Screen Name",
+            selector: (row) => row.userName,
+            sortable: true
+        },
+        {
+            name: "Type",
+            selector: (row) => row.type,
+            sortable: true
+        },
+        {
+            name: "Company Id",
+            selector: (row) => row.userId,
+            sortable: true
+        },
+        {
+            name: "Actions",
+            cell: row => <button className="btn btn-primary" onClick={() => alert(row.screenName)}>Edit</button>
+        }
+    ];
+
+    const tableRef = useRef(null);
+    const { onDownload } = useDownloadExcel({
+        currentTableRef: tableRef.current,
+        filename: 'roles table',
+        sheet: 'roles'
+    })
+
     return (
-        <div>RoleList</div>
+        <>
+            <button className="btn-primary btn btn-sm" onClick={<Finance></Finance>}>Add Role</button>
+            <DataTable
+                title="Role List"
+                columns={column}
+                data={roles}
+                pagination
+                fixedHeader
+                fixedHeaderScrollHeight="25rem"
+                selectableRows
+                selectableRowsHighlight
+                highlightOnHover
+                actions={<button onClick={onDownload} className="btn btn-sm btn-info">Export</button>}
+                subHeader
+                subHeaderComponent={
+                    <input
+                        type="text"
+                        placeholder="Search here"
+                        className="w-25 form-control"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+
+                    />}
+                subHeaderAlign="left"
+            />
+        </>
     )
 }
--- src/component/UserList.jsx
+++ src/component/UserList.jsx
@@ -1,35 +1,101 @@
-import React from 'react'
-import { useState } from "react"
-import axios from "axios"
-import { useEffect } from "react"
+import React, { useRef } from 'react';
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import DataTable from 'react-data-table-component';
+import { useDownloadExcel } from 'react-export-table-to-excel';
+
 export const UserList = () => {
+
+
     const [users, setUsers] = useState([]);
+    const [search, setSearch] = useState("");
+    const [FilterUsers, setFilterUsers] = useState([]);
 
     useEffect(() => {
-        let data = []
-        axios.get('http://localhost:8080/api/v1/user').then((response) => {
-            console.log(response.data);
-            response.data.map((user) => {
-                console.log(user);
-                data.push(user)
-            })
-            console.log("data...")
-            console.log(data)
-            setUsers(data);
-        });
+        async function fetchUserList() {
+            let user = await axios.get('http://localhost:9090/api/v1/user')
+            console.log("--------" + user);
+            setUsers(user.data);
+            setFilterUsers(user.data);
+        };
+        fetchUserList();
+        
     }, []);
 
+    // useEffect(() => {
+    //     const result = users.filter(user => {
+    //         return user.name.toLowerCase().match(search.toLowerCase());
+    //     })
+    //     setFilterUsers(result);
+    //     console.log("-------++++------" + users);
+    // }, [search]);
+
+    const column = [
+        {
+            name: "User Id",
+            selector: (row) => row.userId,
+            sortable: true
+        },
+        {
+            name: "First Name",
+            selector: (row) => row.firstName+row.lastName,
+            sortable: true
+        },
+        {
+            name: "Screen Name",
+            selector: (row) => row.screenName,
+            sortable: true
+        },
+        {
+            name: "Email Address",
+            selector: (row) => row.emailAddress,
+            sortable: true
+        },
+        {
+            name: "Company Id",
+            selector: (row) => row.companyId,
+            sortable: true
+        },
+        {
+            name: "Actions",
+            cell: row => <button className="btn btn-primary" onClick={() => alert(row.screenName)}>Edit</button>
+        }
+    ];
+
+    const tableRef = useRef(null);
+    const { onDownload } = useDownloadExcel({
+        currentTableRef: tableRef.current,
+        filename: 'Users table',
+        sheet: 'Users'
+    })
+
     return (
-        <div>
-            {
-                users.map((user) =>
-                    <>
-                        {
-                            user.firstName
-                        }
-                    </>
-                )
-            }
-        </div>
+        <>
+            <div>hello {users.screenName}</div>
+            <DataTable
+                title="User List"
+                columns={column}
+                data={users}
+                pagination
+                fixedHeader
+                fixedHeaderScrollHeight="25rem"
+                selectableRows
+                selectableRowsHighlight
+                highlightOnHover
+                actions={<button onClick={onDownload} className="btn btn-sm btn-info">Export</button>}
+                subHeader
+                subHeaderComponent={
+                    <input
+                        type="text"
+                        placeholder="Search here"
+                        className="w-25 form-control"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+
+                    />}
+                subHeaderAlign="left"
+            />
+        </>
     )
 }
