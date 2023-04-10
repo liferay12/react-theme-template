61be8c47a5d935f8f3631b28015f23938d88494d
Lohita Sagar Mishra <lohitamishra77@gmail.com>
2023-04-10 10:57:28 +0530

    data table column with json

---
+++ src/theme/Service/notification.jsx
@@ -0,0 +1,5 @@
+export const Notifications= {
+    name:"",
+    subject:"",
+    date:""
+}
--- src/theme/component/Notification.jsx
+++ src/theme/component/Notification.jsx
@@ -1,6 +1,26 @@
 export const Notification=()=>{
     return(
         <>
+            {/* <div class="dropdown-menu dropdown-menu-end"  id="notification">
+                <div id="DZ_W_Notification1" class="widget-media dz-scroll p-3" style={{height:"380px"}}>
+                    <ul class="timeline">
+                        <li>
+                            <div class="timeline-panel">
+                                <div class="media me-2">
+                                    <img alt="image" width="50" src={require('../images/1.jpg')} />
+                                </div>
+                                <div class="media-body">
+                                    <h6 class="mb-1">Dr sultads Send you Photo</h6>
+                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <a class="all-notification" href="javascript:void(0);">See all notifications <i class="ti-arrow-end"></i></a>
+            </div> */}
+
+
         {/* <li class="nav-item dropdown notification_dropdown">
                 <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
                     
