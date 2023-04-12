52c3ddd528be081a98c90d223ffd3ff9baf4f23a
Lohita Sagar Mishra <lohitamishra77@gmail.com>
2023-04-12 19:47:17 +0530

    notification, setting services

--- src/theme/component/Notification.jsx
+++ src/theme/component/Notification.jsx
@@ -1,32 +1,45 @@
-export const Notification=()=>{
-    return(
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export const Notification = () => {
+    const [notifications, setNotifications] = useState([]);
+    useEffect(() => {
+        async function fetchNotifications() {
+            let comments = await axios.get('https://restcountries.com/v2/all')
+            setNotifications(comments.data);
+
+        };
+        fetchNotifications();
+    }, []);
+
+    
+    return (
         <>
-            {/* <div class="dropdown-menu dropdown-menu-end"  id="notification">
-                <div id="DZ_W_Notification1" class="widget-media dz-scroll p-3" style={{height:"380px"}}>
+            <div class="dropdown-menu dropdown-menu-end" id="notification">
+                <div id="DZ_W_Notification1" class="widget-media dz-scroll p-3" style={{ height: "380px" }}>
                     <ul class="timeline">
-                        <li>
-                            <div class="timeline-panel">
-                                <div class="media me-2">
-                                    <img alt="image" width="50" src={require('../images/1.jpg')} />
+                        {notifications.map(note => (  
+                            <li>
+                                <div class="timeline-panel">
+                                    <div class="media me-2">
+                                        <img alt="image" width="50" src={note.flag} />
+                                    </div>
+                                    <div class="media-body">
+                                        <h6 class="mb-1">{note.name}</h6>
+                                        <small class="d-block">{note.area}</small>
+                                    </div>
                                 </div>
-                                <div class="media-body">
-                                    <h6 class="mb-1">Dr sultads Send you Photo</h6>
-                                    <small class="d-block">29 July 2020 - 02:26 PM</small>
-                                </div>
-                            </div>
-                        </li>
+                            </li>   
+                        ))}
                     </ul>
                 </div>
                 <a class="all-notification" href="javascript:void(0);">See all notifications <i class="ti-arrow-end"></i></a>
-            </div> */}
+            </div>
 
 
-        {/* <li class="nav-item dropdown notification_dropdown">
-                <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
-                    
-                </a> */}
-            <div class="dropdown-menu dropdown-menu-end"  id="notification">
-                <div id="DZ_W_Notification1" class="widget-media dz-scroll p-3" style={{height:"380px"}}>
+
+            {/* <div class="dropdown-menu dropdown-menu-end" id="notification">
+                <div id="DZ_W_Notification1" class="widget-media dz-scroll p-3" style={{ height: "380px" }}>
                     <ul class="timeline">
                         <li>
                             <div class="timeline-panel">
@@ -61,10 +74,10 @@ export const Notification=()=>{
                                 </div>
                             </div>
                         </li>
-                            <li>
+                        <li>
                             <div class="timeline-panel">
                                 <div class="media me-2">
-                                    <img alt="image" width="50" src={require('../images/2.jpg')}/>
+                                    <img alt="image" width="50" src={require('../images/2.jpg')} />
                                 </div>
                                 <div class="media-body">
                                     <h6 class="mb-1">Dr sultads Send you Photo</h6>
@@ -127,10 +140,10 @@ export const Notification=()=>{
                                 </div>
                             </div>
                         </li>
-                            <li>
+                        <li>
                             <div class="timeline-panel">
                                 <div class="media me-2">
-                                    <img alt="image" width="50" src={require('../images/5.jpg')}/>
+                                    <img alt="image" width="50" src={require('../images/5.jpg')} />
                                 </div>
                                 <div class="media-body">
                                     <h6 class="mb-1">Dr sultads Send you Photo</h6>
@@ -163,8 +176,8 @@ export const Notification=()=>{
                     </ul>
                 </div>
                 <a class="all-notification" href="javascript:void(0);">See all notifications <i class="ti-arrow-end"></i></a>
-            </div>
-            {/* </li> */}
+            </div> */}
+
         </>
     );
 }
--- src/theme/component/Setting.jsx
+++ src/theme/component/Setting.jsx
@@ -1,18 +1,44 @@
-export const Setting = (item) => {
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const Setting = () => {
+
+  const [randomColor, setRandomColor]=useState(["info", "primary", "danger", "success","warning"])
+  const [setting, setSetting] = useState([]);
+  useEffect(() => {
+    async function fetchSetting() {
+      let setting = await axios.get('https://jsonplaceholder.typicode.com/posts/1/comments')
+      setSetting(setting.data);
+
+    };
+    fetchSetting();
+  }, []);
+
   return (
     <>
-      {/* <li class="nav-item  ">
-                <a class="nav-link " href={item.path} data-bs-toggle="Setting">
-                
-                 {item.name}
-                </a> */}
+
       <div class="dropdown-menu dropdown-menu-end" id="setting">
         <div
           id="DZ_W_TimeLine02"
           class="widget-timeline dz-scroll style-1 p-3 height370"
         >
           <ul class="timeline">
-            <li>
+            {setting.map(set => (
+              <>
+                <li>
+                  <div class={`timeline-badge ${randomColor[Math.floor((Math.random()*randomColor.length))]}`} ></div>{" "}
+                  <a class="timeline-panel text-muted" href="javascript:void(0);">
+                    {" "}
+                    <span>10 minutes ago</span>
+                    <h6 class="mb-0">
+                      Youtube, a video-sharing website, goes live{" "}
+                      <strong class="text-primary">$500</strong>.
+                    </h6>
+                  </a>
+                </li>
+              </>
+            ))}
+            {/* <li>
               <div class="timeline-badge primary"></div>{" "}
               <a class="timeline-panel text-muted" href="javascript:void(0);">
                 {" "}
@@ -74,11 +100,11 @@ export const Setting = (item) => {
                   Mashable, a news website and blog, goes live.
                 </h6>
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
-      {/* </li> */}
+
     </>
   );
 };
